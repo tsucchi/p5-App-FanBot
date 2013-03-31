0450b2c01443f36bbf208b8d36cbeef53a106798
Takuya Tsuchida <tsucchi@cpan.org>
2013-03-31 22:53:40 +0900

    initial commit

---
+++ lib/App/FanBot.pm
@@ -0,0 +1,32 @@
+package App::FanBot;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::FanBot -
+
+=head1 SYNOPSIS
+
+  use App::FanBot;
+
+=head1 DESCRIPTION
+
+App::FanBot is
+
+=head1 AUTHOR
+
+takuya.tsuchida E<lt>tsucchi {at} cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
